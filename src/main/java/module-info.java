86cdf38ddd09cf02c9dae9420e006d9e015fcc82
Mikhail Karmishin <20360770+karmishin@users.noreply.github.com>
2021-05-06 11:58:55 +0300

    shopping list uploading

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,13 +1,14 @@
 module xyz.karmishin.pharmacytracker {
     exports xyz.karmishin.pharmacytracker;
 
-    opens xyz.karmishin.pharmacytracker.entities to javafx.base;
+    opens xyz.karmishin.pharmacytracker.entities to javafx.base, com.fasterxml.jackson.databind;
     opens xyz.karmishin.pharmacytracker.controllers to javafx.fxml;
 
     requires java.net.http;
     requires transitive javafx.controls;
     requires transitive javafx.fxml;
     requires transitive javafx.web;
+    requires transitive javafx.swing;
     requires org.jsoup;
     requires com.fasterxml.jackson.databind;
     requires org.apache.logging.log4j;
@@ -15,4 +16,6 @@ module xyz.karmishin.pharmacytracker {
     requires selenium.api;
     requires selenium.java;
     requires selenium.support;
+    requires com.google.zxing;
+    requires com.google.zxing.javase;
 }
--- src/main/java/xyz/karmishin/pharmacytracker/controllers/ShoppingListController.java
+++ src/main/java/xyz/karmishin/pharmacytracker/controllers/ShoppingListController.java
@@ -1,22 +1,46 @@
 package xyz.karmishin.pharmacytracker.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.qrcode.QRCodeWriter;
 import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.ListChangeListener;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
 import javafx.concurrent.Worker;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import xyz.karmishin.pharmacytracker.entities.ShoppingListEntry;
 
+import java.net.URI;
 import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.ResourceBundle;
+import java.util.UUID;
 
 public class ShoppingListController implements Initializable {
     private SimpleListProperty<ShoppingListEntry> shoppingListProperty;
+    private static Logger logger = LogManager.getLogger();
 
     @FXML
     private TableView<ShoppingListEntry> tableView;
@@ -28,6 +52,8 @@ public class ShoppingListController implements Initializable {
     private TableColumn<ShoppingListEntry, String> address;
     @FXML
     private WebView webView;
+    @FXML
+    private Button exportButton;
 
     public ShoppingListController(SimpleListProperty<ShoppingListEntry> shoppingListProperty) {
         this.shoppingListProperty = shoppingListProperty;
@@ -67,5 +93,91 @@ public class ShoppingListController implements Initializable {
                         "]).addTo(map).bindPopup('" + shoppingListEntry.getAddress() + "');");
             });
         });
+    }
+
+    @FXML
+    protected void handleExportButtonAction() {
+        var list = shoppingListProperty.get();
+        var mapper = new ObjectMapper();
+        try {
+            logger.debug("Processing JSON...");
+            var json = mapper.writeValueAsString(list);
+            logger.debug("Generated JSON: " + json);
+
+            var uploadService = new ShoppingListUploadService(json);
+            uploadService.setOnSucceeded(event -> {
+                logger.debug("Upload successful");
+                showQrCode(uploadService.getValue().uri().toString());
+                exportButton.setDisable(false);
+            });
+            uploadService.setOnRunning(event -> {
+                exportButton.setDisable(true);
+            });
+            uploadService.setOnFailed(event -> {
+                logger.error(uploadService.getException().getMessage());
+                new Alert(Alert.AlertType.ERROR, uploadService.getException().getMessage());
+                exportButton.setDisable(false);
+            });
+
+            uploadService.start();
+            logger.debug("Upload service started");
+        } catch (JsonProcessingException jsonProcessingException) {
+            new Alert(Alert.AlertType.ERROR, jsonProcessingException.getMessage()).show();
+        }
+    }
+
+    private void showQrCode(String content) {
+        int width = 300;
+        int height = 300;
+
+        try {
+            var qrCodeWriter = new QRCodeWriter();
+            logger.debug("Generating QR code...");
+            var bitMatrix = qrCodeWriter.encode(content, BarcodeFormat.QR_CODE, width, height);
+            logger.debug("Creating buffered image...");
+            var bufferedImage = MatrixToImageWriter.toBufferedImage(bitMatrix);
+            logger.debug("Converting to JavaFX image...");
+            var fxImage = SwingFXUtils.toFXImage(bufferedImage, null);
+
+            logger.debug("QR code is ready. Creating the stage...");
+            var borderPane = new BorderPane(new ImageView(fxImage));
+            var scene = new Scene(borderPane);
+            var stage = new Stage();
+            stage.setScene(scene);
+            stage.show();
+            logger.debug("Stage is visible");
+        } catch (WriterException e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+        }
+    }
+
+    private class ShoppingListUploadService extends Service<HttpResponse<String>> {
+        private SimpleStringProperty jsonToSend = new SimpleStringProperty();
+
+        public ShoppingListUploadService(String json) {
+            jsonToSend.set(json);
+        }
+
+        @Override
+        protected Task<HttpResponse<String>> createTask() {
+            var task = new Task<HttpResponse<String>>() {
+                @Override
+                protected HttpResponse<String> call() throws Exception {
+                    var uuid = UUID.randomUUID().toString();
+                    logger.debug("Generated UUID: " + uuid);
+                    var request = HttpRequest.newBuilder()
+                            .uri(URI.create("https://api.cl1p.net/" + uuid))
+                            .POST(HttpRequest.BodyPublishers.ofString(jsonToSend.get()))
+                            .build();
+                    var client = HttpClient.newHttpClient();
+                    logger.debug("Sending request...");
+                    var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                    logger.debug("Request sent");
+                    return response;
+                }
+            };
+
+            return task;
+        }
     }
 }
